411df14e376d05e400a689012adfd8ac6e1a1716
sasmitasahooss <sahoosasmita5420@gmail.com>
2024-11-14 11:15:39 +0530

    Ready

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require('path');
+const _dirname = path.resolve();
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = require('./Database/mongooseConnection');
+const employeesRoute = require('./routes/employeesRoute');
+const adminRoute = require('./routes/adminroute');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(_dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, '/frontend/dist/index.html'));
+});
+app.get('/', (req, res) => {
+  res.send("Hello World")
+});
+
+app.use('/employees', employeesRoute);
+app.use('/admin', adminRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import Admin from './Pages/Admin'
+import Employee from './Pages/Employee'
+import LogIn from './Pages/LogIn'
+import Registration from './Pages/Registration'
+import axios from 'axios';
+function App() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async ({ email, password }) => {
+    if (email === 'admin@example.com' && password === '123') {
+      setUser('admin');
+      navigate('/admin');
+    } else {
+      const response = await axios.post('http://localhost:5000/employees/login',
+        { email, password });  
+      const result = response.data;
+      if (result) {
+        setUser('employee');
+      } else {
+        alert("Invalid credentials");
+      }
+      console.log(user)
+      if(user === 'admin'){
+        navigate('/admin');
+      }else{
+        navigate('/employee');
+      }
+    }
+  };
+
+  return (
+    <Routes>
+      <Route path="/" element={<LogIn handleLogin={handleLogin} />} />
+      <Route path="/login" element={<LogIn handleLogin={handleLogin} />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/employee" element={<Employee />} />
+    </Routes>
+  )
+}
+
+export default App
