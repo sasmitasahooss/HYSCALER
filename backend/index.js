8163265eae873b54c257b2e5c87a66d5cdd64c5c
sasmitasahooss <sahoosasmita5420@gmail.com>
2024-11-14 14:25:31 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
-const _dirname = path.resolve();
 
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -14,13 +13,6 @@ const adminRoute = require('./routes/adminroute');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(_dirname, '/frontend/dist')));
-app.get('*', (_, res) => {
-  res.sendFile(path.join(_dirname, '/frontend/dist/index.html'));
-});
-app.get('/', (req, res) => {
-  res.send("Hello World")
-});
 
 app.use('/employees', employeesRoute);
 app.use('/admin', adminRoute);
